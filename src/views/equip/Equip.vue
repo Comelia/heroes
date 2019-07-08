98fe4b7122496efa43073e0e6345d618a81a1e7d
Comelia <1511867031@qq.com>
2019-07-08 21:27:44 +0800

    修改 weaponlist 和 equiplist

--- src/views/equip/Equip.vue
+++ src/views/equip/Equip.vue
@@ -1,12 +1,34 @@
 <template>
-    <div>
-        装备
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <h2 class="sub-header text-left">装备列表</h2>
+    <a class="btn btn-success" href="add.html">Add</a>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>名称</th>
+            <th>性别</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>###</td>
+            <td>暂无</td>
+            <td>暂无</td>
+            <td>
+              <a href="#">编辑</a>
+              &nbsp;&nbsp;
+              <a href="javascript:window.confirm('Are you sure?')">删除</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
-
+export default {};
 </script>
--- src/views/weapon/Weapon.vue
+++ src/views/weapon/Weapon.vue
@@ -1,7 +1,32 @@
 <template>
-    <div>
-        武器装备
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <h2 class="sub-header text-left">武器列表</h2>
+    <a class="btn btn-success" href="add.html">Add</a>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>名称</th>
+            <th>性别</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>0</td>
+            <td>暂无</td>
+            <td>暂无</td>
+            <td>
+              <a href="#">编辑</a>
+              &nbsp;&nbsp;
+              <a href="javascript:window.confirm('Are you sure?')">删除</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <<script>
